846c7485d6483bf7ff1c51e6f622f3d6254cf52a
Gary Norman <gary.norman.th@gmail.com>
2023-08-28 13:35:00 +0100

    added js animation, changed info box

--- src/virgo.js
+++ src/virgo.js
@@ -1,1 +1,18 @@
-let infoText = document.getElementById('infoText');
+const infoText = document.getElementById('infoText');
+const infoTextClose = document.getElementById('close');
+function infoSlideOpen() {
+    document.getElementById('infoText').style.height = "95%";
+    document.getElementById('infoText').style.width = "96%";
+    document.getElementById('infoText').style.left = "20px";
+    document.getElementById('infoText').style.zIndex = "1";
+    document.getElementById('infoText').style.bottom = "20px";
+    document.getElementById('infoText').style.left = "20px";
+};
+function infoSlideClose() {
+    document.getElementById('infoText').style.height = "0%";
+    document.getElementById('infoText').style.width = "0%";
+    document.getElementById('infoText').style.left = "50%";
+    document.getElementById('infoText').style.zIndex = "-1";
+    document.getElementById('infoText').style.bottom = "40px";
+    document.getElementById('infoText').style.left = "40px";
+};
