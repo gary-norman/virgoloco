5193b45a2381f17579dbacbffe146d5a237b45ec
Gary Norman <gary.norman.th@gmail.com>
2023-08-28 15:04:27 +0100

    made site responsive

--- src/virgo.js
+++ src/virgo.js
@@ -1,18 +1,15 @@
-const infoText = document.getElementById('infoText');
-const infoTextClose = document.getElementById('close');
+const infText = document.getElementById('infoText');
 function infoSlideOpen() {
     document.getElementById('infoText').style.height = "95%";
-    document.getElementById('infoText').style.width = "96%";
-    document.getElementById('infoText').style.left = "20px";
+    document.getElementById('infoText').style.width = "95%";
     document.getElementById('infoText').style.zIndex = "1";
-    document.getElementById('infoText').style.bottom = "20px";
-    document.getElementById('infoText').style.left = "20px";
+    document.getElementById('infoText').style.bottom = "3vw";
+    document.getElementById('infoText').style.left = "3vw";
 };
 function infoSlideClose() {
     document.getElementById('infoText').style.height = "0%";
     document.getElementById('infoText').style.width = "0%";
-    document.getElementById('infoText').style.left = "50%";
     document.getElementById('infoText').style.zIndex = "-1";
-    document.getElementById('infoText').style.bottom = "40px";
-    document.getElementById('infoText').style.left = "40px";
+    document.getElementById('infoText').style.bottom = "6vw";
+    document.getElementById('infoText').style.left = "3vw";
 };
