30aa65325a3f9440eb812515f678e191adc34b5e
Gary Norman <gary.norman.th@gmail.com>
2023-08-29 11:23:20 +0100

    clean up of infotext

--- src/virgo.js
+++ src/virgo.js
@@ -1,11 +1,12 @@
 const infText = document.getElementById('infoText');
 function infoSlideOpen() {
-    document.getElementById('infoText').style.height = "95%";
+    document.getElementById('infoText').style.height = "90%";
     document.getElementById('infoText').style.width = "95%";
     document.getElementById('infoText').style.zIndex = "1";
     document.getElementById('infoText').style.bottom = "3vw";
     document.getElementById('infoText').style.left = "3vw";
     document.getElementById('infoText').style.margin = "0px 0px";
+    document.getElementById('infoText').style.border = "2px";
 };
 function infoSlideClose() {
     document.getElementById('infoText').style.height = "0%";
@@ -14,4 +15,5 @@ function infoSlideClose() {
     document.getElementById('infoText').style.bottom = "6vw";
     document.getElementById('infoText').style.left = "3vw";
     document.getElementById('infoText').style.margin = "0px 10px";
+    document.getElementById('infoText').style.border = "0px";
 };
