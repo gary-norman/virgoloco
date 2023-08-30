5e69009e18e6ab209852ed7801ddb8a1d39b071c
Gary Norman <gary.norman.th@gmail.com>
2023-08-30 14:28:36 +0100

    cleanup

--- src/virgo.js
+++ src/virgo.js
@@ -1,10 +1,8 @@
 const infText = document.getElementById('infoText');
 function infoSlideOpen() {
-    document.getElementById('infoText').style.height = "90%";
+    document.getElementById('infoText').style.height = "95%";
     document.getElementById('infoText').style.width = "95%";
     document.getElementById('infoText').style.zIndex = "1";
-    document.getElementById('infoText').style.bottom = "3vw";
-    document.getElementById('infoText').style.left = "3vw";
     document.getElementById('infoText').style.margin = "0px 0px";
     document.getElementById('infoText').style.border = "2px";
 };
@@ -12,8 +10,6 @@ function infoSlideClose() {
     document.getElementById('infoText').style.height = "0%";
     document.getElementById('infoText').style.width = "0%";
     document.getElementById('infoText').style.zIndex = "-1";
-    document.getElementById('infoText').style.bottom = "6vw";
-    document.getElementById('infoText').style.left = "3vw";
     document.getElementById('infoText').style.margin = "0px 10px";
     document.getElementById('infoText').style.border = "0px";
 };
